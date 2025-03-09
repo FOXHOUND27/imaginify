ad485b94c42759088c77a83903ff6322549e3798
dalglish <dalglish45@gmail.com>
2025-03-09 15:19:51 +0200

    commiting partially completed sidebar

--- components/shared/SideBar.tsx
+++ components/shared/SideBar.tsx
@@ -1,8 +1,13 @@
+"use client";
+import { navLinks } from "@/data/constants";
+import { SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const SideBar = () => {
+  const pathname = usePathname();
   return (
     <aside className="sidebar">
       <div className="flex size-full flex-col gap-4">
@@ -15,6 +20,29 @@ const SideBar = () => {
             height={28}
           />
         </Link>
+
+        {/* navigation */}
+        <nav className="sidebar-nav">
+          <SignedIn>
+            <ul className="sidebar-nav_elements">
+              {navLinks.map((link) => {
+                const isActive = link.route === pathname;
+                return (
+                  <li
+                    key={link.route}
+                    className={`sidebar-nav_element group ${
+                      isActive
+                        ? "bg-purple-gradient text-white"
+                        : "text-gray-700"
+                    }`}
+                  >
+                    {link.label}
+                  </li>
+                );
+              })}
+            </ul>
+          </SignedIn>
+        </nav>
       </div>
     </aside>
   );
