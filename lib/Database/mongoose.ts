045f2afae0df0760850ea7d1721ba399bb3e3f23
dalglish <dalglish45@gmail.com>
2025-03-10 20:29:41 +0200

    commiting code

--- lib/Database/mongoose.ts
+++ lib/Database/mongoose.ts
@@ -1,8 +1,19 @@
 import mongoose, { Mongoose } from "mongoose";
 
 const MONGODB_URL = process.env.MONGODB_URL;
-
 interface MongooseConnection {
   conn: Mongoose | null;
-  promise: Promise<Mongoose | null>;
+  promise: Promise<Mongoose> | null;
 }
+
+let cached: MongooseConnection = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+export const connectToDb = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!MONGODB_URL) throw new Error("MONGODB_URL is not defined");
+};
