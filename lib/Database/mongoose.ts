1deaa1561af5c32a69827488bbb958951bcb90bf
dalglish <dalglish45@gmail.com>
2025-03-10 21:14:50 +0200

    commiting database setup and schemas

--- lib/Database/mongoose.ts
+++ lib/Database/mongoose.ts
@@ -16,4 +16,15 @@ export const connectToDb = async () => {
   if (cached.conn) return cached.conn;
 
   if (!MONGODB_URL) throw new Error("MONGODB_URL is not defined");
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URL, {
+      dbName: "Imaginify",
+      bufferCommands: false,
+    });
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 };
