4dc995b5938d415a4b92470d7ba4fa5b31ff1c2e
dalglish <dalglish45@gmail.com>
2025-03-11 13:06:13 +0200

    commiting code

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -2,11 +2,15 @@ import { Header } from "@/components/shared/Header";
 import React from "react";
 import { SearchParamProps } from "@/types";
 import { transformationTypes } from "@/data/constants";
+import TransformationForm from "@/components/Forms/TransformationForm";
 
 const AddTranformationTypePage = ({ params: { type } }: SearchParamProps) => {
   const transformatiom = transformationTypes[type];
   return (
-    <Header title={transformatiom.title} subTitle={transformatiom.subTitle} />
+    <>
+      <Header title={transformatiom.title} subTitle={transformatiom.subTitle} />
+      <TransformationForm />
+    </>
   );
 };
 
